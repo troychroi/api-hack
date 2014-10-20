a048505a9e804ef9e1139c6e938c783087f895a5
Troy Church <tchurch@berklee.edu>
2014-10-19 22:18:56 -0400

    refresh

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ $(document).keyup(function(event) {
 		if (event.which == 13) {
 			tags = $("input[name='tags']").val();
 			getData();
-			var endpoint = getData();
-			//$("img").attr('src', "https://farm"+ endpoint.photos.photo[0].farm +".staticflickr.com/"+ endpoint.photos.photo[0].server +"/"+ endpoint.photos.photo[0].id +"_"+ endpoint.photos.photo[0].secret +"m.jpg");
-			console.log(endpoint);
+			result = getData();
+			//$("img").attr('src', "https://farm"+ result.photos.photo[0].farm +".staticflickr.com/"+ result.photos.photo[0].server +"/"+ result.photos.photo[0].id +"_"+ result.photos.photo[0].secret +"m.jpg");
 			console.log(tags);
+			console.log(result);
 		}
 });
 var getData = function() {  
@@ -23,7 +23,8 @@ var getData = function() {
 		content_type: "1",
 		per_page: "100",
 		method: "flickr.photos.search",
-		api_key: "eadba36bed227c4d0b98d9f47b7b2a18"
+		api_key: "eadba36bed227c4d0b98d9f47b7b2a18",
+		format: "json"
 	};
 
 	var result =  $.ajax({
@@ -32,7 +33,7 @@ var getData = function() {
 			data: args,
 			type: "GET"
 			})
-	console.log(result);
+	return result;
 }
 
 });
