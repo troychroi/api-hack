9d0e8fd5d2b662f74a38c790dcadf364a3ea560c
Troy Church <tchurch@berklee.edu>
2014-10-22 01:32:45 -0400

    done

--- copy.js
+++ copy.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
 
+var random = Math.floor((Math.random() * 10) + 1);
+var ideas = [
+    "zombie",
+    "Lady Gaga",
+    "bumblebee",
+    "ghost",
+    "Link from Zelda",
+    "sexy Santa Claus",
+    "werewolf",
+    "pirate",
+    "Spiderman",
+    "Miley Cyrus"
+];
+
+$('input[name="tags"]').attr('placeholder', ideas[random]);
+
 $('form').submit(function(event) {
     event.preventDefault();
       $('#results').find('img').remove();
@@ -10,10 +26,10 @@ function getData() {
   tags = $("input[name='tags']").val();
   var args = {
     tags: tags + ", halloween, costume",
-    text: tags + ", halloween, costume",
-    accuracy: "1",
+    text: tags + ", halloween, costume, "+ tags +" halloween costume, "+ tags +" costume, "+ tags +" for halloween",
+    accuracy: "3",
     content_type: "1",
-    per_page: "100",
+    per_page: "300",
     method: "flickr.photos.search",
     api_key: "eadba36bed227c4d0b98d9f47b7b2a18",
     format: "json"
